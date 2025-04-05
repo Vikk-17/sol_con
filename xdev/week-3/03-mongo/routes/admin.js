804b53308bc77fbb44d9c51f9c39d6861ad1a542
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-04-05 18:27:09 +0530

    admin route complete

--- xdev/week-3/03-mongo/routes/admin.js
+++ xdev/week-3/03-mongo/routes/admin.js
@@ -20,27 +20,32 @@ router.post('/signup', async (req, res) => {
 });
 
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
     const title = req.body.title;
     const description = req.body.description;
     const imageLink = req.body.imageLink;
     const price = req.body.price;
 
-    Course.create({
+    const newCourse = await Course.create({
         title,
         description,
         imageLink,
         price
-    }).then(()=>{
-        res.json({
-            message: "Course created successfully"
-        })
+    });
+    res.json({
+        message: "Course created successfully",
+        courseId: newCourse._id,
     })
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+    const response = await Course.find({});
+    res.json({
+        course: response,
+    })
 });
 
 module.exports = router;
